dfbb6fe4ec0dde7479e009d1916b2a7bdb56cf4a
Sehar-Tahir <sehartahirofficial@gmail.com>
2025-10-28 13:13:23 +0500

    frontend setup done ( React Js with Tailwind css)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="h-screen text-red-600 flex items-center justify-center bg-gray-900 text-3xl font-bold">
+        Tailwind + React + Vite setup complete 🎉
+      </div>
+    </>
+  )
+}
+
+export default App

8dfe808f3575547f8ae33919654af7dd93269b62
OjulaRoseline <roselineakinyi587@gmail.com>
2025-10-28 16:25:39 +0300

    feat(contracts): remove OZ deps; add local Ownable/ReentrancyGuard/IERC20 and update imports in PaymentRouter/EscrowContract

--- contracts/src/EscrowContract.sol
+++ contracts/src/EscrowContract.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./IERC20.sol";
+import "./ReentrancyGuard.sol";
 
 /**
  * @title EscrowContract
--- contracts/src/PaymentRouter.sol
+++ contracts/src/PaymentRouter.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IERC20.sol";
+import "./ReentrancyGuard.sol";
+import "./Ownable.sol";
 
 /**
  * @title PaymentRouter
